40533359cc082eab1f37865b567ff561418da41a
frohlichcortezh <frohlichcortezh@protonmail.com>
2020-11-10 23:27:33 +0100

    Added new packages

--- init.sh
+++ init.sh
@@ -23,6 +23,32 @@ source setup-repositories.sh
 f_pkg_manager_update
 
 # ToDo break it into different categories and according to type of installation
-f_app_install refind keepassxc snapd
+f_app_install refind keepassxc snapd tightvncserver net-tools xrdp ssh gdebi gufw gnome-tweaks guake gnome-shell-extensions firefox python3-nautilus python3-pip apt-transport-https 
+f_app_install_from_snap code --classic
+f_app_install_from_snap spotify
+# https://github.com/flozz/nautilus-terminal
+f_app_install_from_pip nautilus_terminal
 
-f_app_install_from_snap code
+#ToDo find url according to distro and version
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O $HOME/dev/packages/packages-microsoft-prod.deb
+sudo dpkg -i $HOME/dev/packages/packages-microsoft-prod.deb
+
+f_pkg_manager_update
+
+f_app_install dotnet-sdk-3.1
+
+# setup
+# inform user of actions to do 
+vncserver
+# finish configuring vncserver https://linoxide.com/tools/install-configure-vnc-ubuntu-20-04/
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-vnc-on-ubuntu-20-04
+
+#configure ssh
+
+# enable services
+sudo systemctl enable xrdp
+
+
+# manual actions
+
+firefox https://extensions.gnome.org/extension/906/sound-output-device-chooser/
---
+++ setup-firefox.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# https://support.mozilla.org/en-US/kb/profiles-where-firefox-stores-user-data
+# https://developer.mozilla.org/en-US/docs/Mozilla/Command_Line_Options
